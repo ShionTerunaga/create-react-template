5b8c390b4c7b3dd24efe6f6acc4ae0fa48800d84
Shion Terunaga <shion.terunaga@ShionnoMacBook-Pro.local>
2025-11-22 17:59:30 +0900

    feat: add copy script

---
+++ execution/mac/tmp/lib/stories/popup-vanilla/popup-base/popup-base.stories.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { PopupBase } from "./popup-base";
+import { popupStore } from "@/lib/popup/store";
+import { usePopup } from "@/lib/popup";
+
+export default {
+    title: "Popup/Vanilla/PopupBase",
+    component: PopupBase
+};
+
+export function ButtonToOpenPopup() {
+    const popup = usePopup();
+
+    return (
+        <button
+            onClick={() =>
+                popup.open(
+                    <div style={{ padding: 16, backgroundColor: "white" }}>
+                        Story popup content
+                        <br />
+                        <button onClick={() => popup.close()}>Close</button>
+                    </div>
+                )
+            }
+        >
+            Open Popup
+        </button>
+    );
+}
+
+export const Default = () => (
+    <>
+        <h1>Underlying Content</h1>
+
+        <ButtonToOpenPopup />
+
+        <PopupBase />
+    </>
+);
