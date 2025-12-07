6675d949febd8a3df589a872e6fe4eb11d9a2bdd
Shion Terunaga <shion.terunaga@ShionnoMacBook-Pro.local>
2025-12-08 01:54:15 +0900

    feat: create lib cli

--- packages/create-template/src/main.ts
+++ packages/create-template/src/main.ts
@@ -1,39 +1,25 @@
 import { basename, resolve } from "node:path";
-import prompts from "prompts";
 import { validateNpmName } from "./helper/validate-npm-name";
 import { existsSync } from "node:fs";
 import { bold, red, green } from "picocolors";
 import { createApp } from "./create-app";
-import { Lib } from "./template-src/template.static";
-import { Option, optionUtility } from "./utils/option";
 import { resultUtility } from "./utils/result";
 import { commanderCore } from "./command/core";
 import { nameCommand } from "./command/name";
 import { frameworkCommand } from "./command/framework";
-import { isLib, isLibsArray } from "./utils/is";
+import { cssCommand } from "./command/css";
+import { libCli } from "./command/lib";
 
 const handleSigTerm = () => process.exit(0);
 
 process.on("SIGTERM", handleSigTerm);
 process.on("SIGINT", handleSigTerm);
 
-interface SelectLib {
-    title: string;
-    value: Lib;
-}
-
-const selectLibList: Array<SelectLib> = [
-    { title: "Popup", value: "popup" },
-    { title: "Loading", value: "loading" }
-];
-
 export async function run(): Promise<string> {
-    const { optionConversion, isNone } = optionUtility;
-    const { createNg, createOk, isNG } = resultUtility;
+    const { isNG } = resultUtility;
 
-    const { optionName, optionFramework, onPromptState } = commanderCore;
-
-    let isTailwind = false;
+    const { optionName, optionFramework, optionCss, optionUseAllComponents } =
+        commanderCore;
 
     const projectName = await nameCommand(optionName);
 
@@ -75,63 +61,30 @@ export async function run(): Promise<string> {
         process.exit(1);
     }
 
-    const { tailwind } = await prompts({
-        onState: onPromptState,
-        type: "toggle",
-        name: "tailwind",
-        message: `Would you like to use tailwindCSS?`,
-        initial: false,
-        active: "Yes",
-        inactive: "No"
-    });
+    const cssResult = await cssCommand(optionCss);
 
-    isTailwind = Boolean(tailwind);
+    if (isNG(cssResult)) {
+        console.error(red(cssResult.err.message));
+        console.error(cssResult.err.stack ?? "");
 
-    const res = await prompts({
-        onState: onPromptState,
-        type: "multiselect",
-        name: "packages",
-        message: "Select packages to add",
-        choices: selectLibList,
-        hint: "(Use space to select, and enter to submit)",
-        instructions: false
-    });
-
-    const optionSelected: Option<unknown> = optionConversion(res.packages);
-
-    if (isNone(optionSelected)) {
-        console.log("No packages selected. Exiting.");
+        process.exit(1);
     }
 
-    const selectedPackages = optionSelected.value;
-
-    const resultSelected = isLibsArray(selectedPackages)
-        ? createOk(selectedPackages)
-        : isLib(selectedPackages)
-          ? createOk([selectedPackages])
-          : createNg(new Error("Selected packages have an invalid structure."));
+    const resultSelected = await libCli(optionUseAllComponents);
 
     if (isNG(resultSelected)) {
-        console.error(resultSelected.err.message);
+        console.error(red(resultSelected.err.message));
         console.error(resultSelected.err.stack ?? "");
 
         process.exit(1);
     }
 
-    try {
-        await createApp({
-            appPath,
-            framework: frameworResult.value,
-            tailwind: isTailwind,
-            libs: resultSelected.value
-        });
-    } catch (e) {
-        console.error(red("An error occurred while creating the project."));
-        if (e instanceof Error) {
-            console.error(red(e.message));
-        }
-        process.exit(1);
-    }
+    await createApp({
+        appPath,
+        framework: frameworResult.value,
+        css: cssResult.value,
+        libs: resultSelected.value
+    });
 
     return projectName.value;
 }
